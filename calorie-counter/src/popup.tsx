11d2aa0d9343a4e61670b7e4c4d8fcbf9d48914a
kabiru <mwangikabiru21@gmail.com>
2024-04-03 19:37:35 +0300

    We are running booooois

--- calorie-counter/src/popup.tsx
+++ calorie-counter/src/popup.tsx
@@ -1,12 +1,16 @@
-import { CountButton } from "~features/count-button"
-
 import "~style.css"
 
 function IndexPopup() {
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <CountButton />
-    </div>
+    <div className="flex flex-col p-8 items-center justify-center h-fit w-80">
+      <p className="text-blue-500 text-2xl">
+        <span className="font-bold">Calorie</span>
+        <span className="underline italic">Counter</span>
+      </p>
+      <p className="text-black text-sm text-center">
+        How much AI content have you consumed today ?
+      </p>
+    </div >
   )
 }
 
