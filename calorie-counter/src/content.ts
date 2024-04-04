9508dc81d5ec8126c39bddc456c3f4f5f4f06ce4
kabiru <mwangikabiru21@gmail.com>
2024-04-04 20:13:14 +0300

    Scrape scrape!

--- calorie-counter/src/content.ts
+++ calorie-counter/src/content.ts
@@ -1,5 +1,21 @@
-export { }
+import type { PlasmoCSConfig } from "plasmo"
 
-console.log(
-  "You may find that having is not so pleasing a thing as wanting. This is not logical, but it is often true."
-)
+export const config: PlasmoCSConfig = {
+  matches: ["<all_urls>"],
+  all_frames: true
+}
+
+const scrapeContent = () => {
+  const title = document.title;
+  const url = window.location.href;
+  console.log(`Scraped content: Title - ${title}, URL - ${url}`);
+};
+
+
+window.addEventListener("load", () => {
+  console.log(
+    "Be one, then be noone. Be noone, then be everyone. Be everyone, then be any one"
+  )
+  scrapeContent();
+})
+
